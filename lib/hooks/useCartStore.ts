4807ee454d8bcdbaac42645424607358e5baaf82
rohit <rohitsb96134@gmail.com>
2024-12-28 15:33:24 +0530

    Implement cart store functionality with item quantity management and price calculation

--- lib/hooks/useCartStore.ts
+++ lib/hooks/useCartStore.ts
@@ -1,7 +1,7 @@
 import {create} from "zustand"
 import { roundoff } from "../utils"
 import { OrderItem } from "../models/orderModel"
-import { persist } from "zustand/middleware"
+// import { persist } from "zustand/middleware"
 
 
 type Cart = {
@@ -11,3 +11,75 @@ type Cart = {
     totalPrice:number,
     items: OrderItem[]
 }
+
+const initialState: Cart = {
+  itemsPrice:0,
+  taxPrice: 0,
+  shippingPrice: 0,
+  totalPrice: 0,
+  items:[]
+};
+
+export const cartStore = create<Cart>(() => initialState)
+
+export default function useCartService(){
+    const {itemsPrice,
+  taxPrice,
+  shippingPrice,
+  totalPrice,
+  items} = cartStore()
+
+  return {
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    items,
+    increase: (item: OrderItem) => {
+      const exist = items.find((x) => x.slug === item.slug);
+      const updatedCartItems = exist
+        ? items.map((x) =>
+            x.slug === item.slug ? { ...exist, qty: exist.qty + 1 } : x
+          )
+        : [...items, { ...item, qty: 1 }];
+      const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+        calcPrice(updatedCartItems);
+      cartStore.setState({
+        items: updatedCartItems,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice,
+      });
+    },
+    decrease: (item: OrderItem) => {
+      const exist = items.find((x) => x.slug === item.slug);
+      if (!exist) return;
+      const updatedCartItems =
+        exist.qty === 1
+          ? items.filter((x: OrderItem) => x.slug !== item.slug)
+          : items.map((x) =>
+              item.slug ? { ...exist, qty: exist.qty - 1 } : x
+            );
+      const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
+        calcPrice(updatedCartItems);
+      cartStore.setState({
+        items: updatedCartItems,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice,
+      });
+    },
+  };
+}
+
+const calcPrice = (items: OrderItem[]) => {
+  const itemsPrice = roundoff(
+      items.reduce((acc, item) => acc + item.price * item.qty, 0)
+    ),
+    shippingPrice = roundoff(itemsPrice > 100 ? 0 : 100),
+    taxPrice = roundoff(Number(0.15 * itemsPrice)),
+    totalPrice = roundoff(itemsPrice + shippingPrice + taxPrice);
+  return { itemsPrice, shippingPrice, taxPrice, totalPrice };
+};
