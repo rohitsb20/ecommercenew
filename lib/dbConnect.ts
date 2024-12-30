4dccc5ac648b6eef8db440cf9d5f9963a6e49cf3
rohit <rohitsb96134@gmail.com>
2024-12-30 14:46:35 +0530

    Fix database connection string syntax in dbConnect function

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
   async function dbconnect(){
     try {
-     await  mongoose.connect("process.env.MONGODB_URI")
+     await mongoose.connect("process.env.MONGODB_URI");
     } catch (error) {
       console.log("failed to connect", error);
       throw new Error("failed to connect");
