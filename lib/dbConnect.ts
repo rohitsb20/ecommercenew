35a21f2670471c407e85782c137a2efc89a50d99
rohit <rohitsb96134@gmail.com>
2024-12-30 14:45:59 +0530

    Add MongoDB and Mongoose dependencies; implement database connection logic

---
+++ lib/dbConnect.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+  async function dbconnect(){
+    try {
+     await  mongoose.connect("process.env.MONGODB_URI")
+    } catch (error) {
+      console.log("failed to connect", error);
+      throw new Error("failed to connect");
+    }
+
+  }
+
+  export default dbconnect;
+
+
+
+  
